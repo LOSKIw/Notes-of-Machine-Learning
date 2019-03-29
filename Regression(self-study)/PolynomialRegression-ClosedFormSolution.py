ef31fabea43242f4de96b57ac9596c3b86f62a0e
LOSKIw <48372803+LOSKIw@users.noreply.github.com>
2019-03-29 23:36:14 +0800

    complete notes

--- Regression(self-study)/PolynomialRegression-ClosedFormSolution.py
+++ Regression(self-study)/PolynomialRegression-ClosedFormSolution.py
@@ -9,6 +9,7 @@ workbook=xlrd.open_workbook(r'6.xls')
 sheet=workbook.sheet_by_index(0)
 cols1=sheet.col_values(0)                          #获取第一列
 cols2=sheet.col_values(1)                          #获取第二列
+#n为数据规模
 n=100
 s1=0
 s2=0
@@ -17,6 +18,7 @@ s4=0
 s5=0
 s6=0
 s7=0
+#准备变量
 for i in range(n):
     s1=s1+cols2[i]
     s2=s2+cols1[i]
@@ -32,17 +34,17 @@ f1=((s1-b1*s2-b2*s3)/100)-b0
 f2=((s4-b0*s2-b2*s5)/s3)-b1
 f3=((s6-b0*s3-b1*s5)/s7)-b2
 result=sp.solve([f1,f2,f3],[b0,b1,b2])
-
-#{b0: 5.54334244651814, b1: 0.458746450400443, b2: 0.960930395945233}
-
-#b0=sp.Symbol('b0')
-#b1=sp.Symbol('b1')
-#b2=sp.Symbol('b2')
+#等价于
 #sp.solve([((s1-b1*s2-b2*s3)/100)-b0,((s4-b0*s2-b2*s5)/s3)-b1,((s6-b0*s3-b1*s5)/s7)-b2],[b0,b1,b2])
+#先前答案：
+#{b0: 5.54334244651814, b1: 0.458746450400443, b2: 0.960930395945233}
+#从结果中提取，根据查找，solve()函数的结果貌似是存储在一个list中的
 a=result[b0]
 b=result[b1]
 c=result[b2]
+#绘制散点图
 plt.scatter(cols1,cols2,color = 'blue')
+#绘制结果图
 x=np.linspace(0,15,100)
 y=a+b*x+c*x*x
 plt.plot(x,y,color="red")
--- Regression(self-study)/SimpleLinearRegression-GradientDescent.py
+++ Regression(self-study)/SimpleLinearRegression-GradientDescent.py
@@ -6,32 +6,33 @@ import numpy as np
 workbook=xlrd.open_workbook(r'1.xls')
 
 sheet=workbook.sheet_by_index(0)
-cols1=sheet.col_values(0)                                            #获取第一列
-cols2=sheet.col_values(1)                                            #获取第二列
+cols1=sheet.col_values(0)                           #获取第一列
+cols2=sheet.col_values(1)                           #获取第二列
 
-#a+bx
+#a+b
 #a=sp.Symbol('a')
 #b=sp.Symbol('b')
-#已知a=-22.63450339669057 b=13.449314363947979
-
+#已知结果约为a=-22.63450339669057 b=13.449314363947979
+#求方差
 def get_sqm(a,b):
     sqm=0
     for i in range(100):
         sqm=sqm+(cols2[i]-a-b*cols1[i])*(cols2[i]-a-b*cols1[i])
     return sqm
-
+#分别准备对a,b求偏导的函数
 def get_pa(a,b):
     pa=0
     for i in range(100):
         pa=pa-2*(cols2[i]-a-b*cols1[i])
     return pa
-
 def get_pb(a,b):
     pb=0
     for i in range(100):
         pb=pb-2*cols1[i]*(cols2[i]-a-b*cols1[i])
     return pb
+#n为逼近时的系数，非常重要
 n=0.0001
+#初始化a,b两值
 a=0.0
 b=0.0
 while abs(get_pa(a,b))>=1 and abs(get_pb(a,b))>=1 :
@@ -40,9 +41,11 @@ while abs(get_pa(a,b))>=1 and abs(get_pb(a,b))>=1 :
     a=a-n*get_pa(c,d)
     b=b-n*get_pb(c,d)
     print(get_sqm(a,b))
-
+#输出结果
 print(a,b)
+#绘制散点图
 plt.scatter(cols1,cols2,color = 'blue')
+#绘制结果图
 x=np.linspace(0,15,100)
 y=b*x+a
 plt.plot(x,y,color="red")
