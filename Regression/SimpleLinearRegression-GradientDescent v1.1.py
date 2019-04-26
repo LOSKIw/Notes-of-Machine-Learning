2cc41af3a000ea9797124db2cfdaad9abefd8ef6
LOSKIw <48372803+LOSKIw@users.noreply.github.com>
2019-04-26 22:43:52 +0800

    regression could print the learning step

---
+++ Regression/SimpleLinearRegression-GradientDescent v1.1.py
@@ -0,0 +1,53 @@
+import xlrd
+import xlwt
+import sympy as sp
+import matplotlib.pyplot as plt
+import numpy as np
+workbook=xlrd.open_workbook(r'1.xls')
+
+sheet=workbook.sheet_by_index(0)
+cols1=sheet.col_values(0)                                            #获取第一列
+cols2=sheet.col_values(1)                                            #获取第二列
+
+#a+bx
+#a=sp.Symbol('a')
+#b=sp.Symbol('b')
+#已知a=-22.63450339669057 b=13.449314363947979
+
+def get_sqm(a,b):
+    sqm=0
+    for i in range(100):
+        sqm=sqm+(cols2[i]-a-b*cols1[i])*(cols2[i]-a-b*cols1[i])
+    return sqm
+
+def get_pa(a,b):
+    pa=0
+    for i in range(100):
+        pa=pa-2*(cols2[i]-a-b*cols1[i])
+    return pa
+
+def get_pb(a,b):
+    pb=0
+    for i in range(100):
+        pb=pb-2*cols1[i]*(cols2[i]-a-b*cols1[i])
+    return pb
+n=0.0001
+a=0.0
+b=0.0
+x=np.linspace(0,15,100)
+#y=b*x+a
+
+while abs(get_pa(a,b))>=1 and abs(get_pb(a,b))>=1 :
+    c=a
+    d=b
+    a=a-n*get_pa(c,d)
+    b=b-n*get_pb(c,d)
+    print(get_sqm(a,b))
+    plt.plot(x,b*x+a,color="green")
+    
+
+print(a,b)
+plt.scatter(cols1,cols2,color = 'blue')
+
+plt.plot(x,b*x+a,color="red")
+plt.show()
